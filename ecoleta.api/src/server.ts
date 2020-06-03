04fc9a3bb55516a3ed4196cead59bab05e1fbeba
Michael Nel <neckware@gmail.com>
2020-06-03 20:26:16 -0300

    Database Config & Seed & Test

--- ecoleta.api/src/server.ts
+++ ecoleta.api/src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
-const app = express();
-app.get('/users',  (request, response) => {
-    response.json("Hello World");
-});
+import path from 'path'
+import routes from './routes'
 
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+
+app.use ('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.listen(3333);
