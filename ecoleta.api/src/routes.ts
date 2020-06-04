be320febe591ed5c7918e9c5938f0df86b2e5cd9
Michael Nel <neckware@gmail.com>
2020-06-03 21:41:13 -0300

    Cors

--- ecoleta.api/src/routes.ts
+++ ecoleta.api/src/routes.ts
@@ -3,11 +3,14 @@ import knex from './database/connection'
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
 const routes = express.Router();
+
+//Constructors
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
-routes.post('/points', pointsController.create);
+routes.get('points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+routes.post('/points', pointsController.create);
 
 export default routes;
