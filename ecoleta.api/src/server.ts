368b56a5c7ef4a1c3a32b31a203b45222cd66276
Michael Nel <neckware@gmail.com>
2020-06-04 15:26:10 -0300

    CreatePoint & Map & Items Api

--- ecoleta.api/src/server.ts
+++ ecoleta.api/src/server.ts
@@ -9,5 +9,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use ('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.listen(3333);
---
+++ ecoleta.web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet'
+import api from '../../services/api';
+import './styles.css';
+import logo from '../../assets/logo.svg'
+
+
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta" />
+                <Link to="/">
+                    <FiArrowLeft />
+                  Voltar para home
+              </Link>
+            </header>
+            <form>
+                <h1>Cadas do <br /> ponto de coleta</h1>
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input type="text" name="text" id="name" />
+                    </div>
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input type="email" name="email" id="email" />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">WhatsApp</label>
+                            <input type="text" name="whatsapp" id="whatsapp" />
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereços</h2>
+                        <span>Selecione o endereço dno mapa </span>
+                    </legend>
+
+                    <Map center={[-23.5962565, -46.5258909]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-23.5962565, -46.5258909]} />
+                    </Map>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf" >Estado (Uf)</label>
+                            <select name="uf" id="uf" >
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Ítens de coleta</h2>
+                        <span>Selecione um ou mais items abaixo</span>
+                    </legend>
+                    <ul className="items-grid">
+                        {items.map(item => (
+                            <li key={item.id} >
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                            </li>
+
+                        ))}
+                    </ul>
+                </fieldset>
+                <button type="submit">Cadastrar ponto de coleta</button>
+            </form>
+
+        </div>
+    );
+}
+
+export default CreatePoint;
